1168543891264c8f3a0ea8d40bbada9ad748bfee
Asphodan <afflictive@gmail.com>
2013-02-16 20:27:12 -0600

    Don't install technic.

--- src/main/java/org/spoutcraft/launcher/util/Utils.java
+++ src/main/java/org/spoutcraft/launcher/util/Utils.java
@@ -72,7 +72,7 @@ public class Utils {
 			}
 
 			if (!exists) {
-				int result = JOptionPane.showConfirmDialog(splash, "No installation of our launcher found. Will install at: " + workDir.getAbsolutePath() + " Would you like to change the install directory?", "Install Technic Launcher", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+				int result = JOptionPane.showConfirmDialog(splash, "No installation of our launcher found. Will install at: " + workDir.getAbsolutePath() + " Would you like to change the install directory?", "Install Launcher", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
 				if (result == JOptionPane.YES_OPTION) {
 					JFileChooser fileChooser = new JFileChooser(workDir);
 					fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
