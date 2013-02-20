747d528830b7daf4fc8e6246121df945b0f82885
Asphodan <afflictive@gmail.com>
2013-02-19 21:25:09 -0600

    Additional transition changes.

--- src/main/java/org/spoutcraft/launcher/skin/ErrorDialog.java
+++ src/main/java/org/spoutcraft/launcher/skin/ErrorDialog.java
@@ -91,8 +91,8 @@ public class ErrorDialog extends JDialog implements ActionListener{
 	}
 
 	private String generateExceptionReport() {
-		StringBuilder builder = new StringBuilder("Technic Launcher Error Report:\n");
-		builder.append("( Please submit this report to https://github.com/TechnicPack/Spoutcraft-Launcher/issues )\n");
+		StringBuilder builder = new StringBuilder("Betterverse Launcher Error Report:\n");
+		builder.append("( Please submit this report to https://github.com/Betterverse/Launcher/issues )\n");
 		builder.append("    Launcher Build: ").append(Settings.getLauncherBuild()).append("\n");
 		builder.append("----------------------------------------------------------------------").append("\n");
 		builder.append("Stack Trace:").append("\n");
